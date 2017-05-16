b790986421a271e51322f4161b570bfc0226e5c1
Bharat Tiwari <kkaphm@live.com>
2017-05-15 21:31:19 -0500

    added tab navigation

---
+++ src/screens/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const headerScreen = () => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle} >
+            <Text style={textStyle}> Header screen </Text>
+        </View>
+    );
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 20
+    },
+    viewStyle: {
+        flex: 1,
+        flexDirection: 'row' as 'row',
+        alignItems: 'center' as 'center',
+        justifyContent: 'center' as 'center'
+    }
+};
+
+export { headerScreen };
